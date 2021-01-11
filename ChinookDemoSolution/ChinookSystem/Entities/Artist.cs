b645131e326eb92601397e2e3c22cb81c8b3578f
aymand <aymand1982@gmail.com>
2021-01-11 13:42:33 -0700

    Add Artist Entity
    Added Artist entity and implemented properties as required

---
+++ ChinookDemoSolution/ChinookSystem/Entities/Artist.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additionl Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace ChinookSystem.Entities
+{
+    [Table("Artists")]
+    internal class Artist
+    {
+        private string _Name;
+
+        [Key]
+        public int ArtistId { get; set; }
+
+        [StringLength(120, ErrorMessage = "Name cannot be longer than 120 characters")]
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = string.IsNullOrEmpty(value) ? null : value;
+            }
+        }
+    }
+}
